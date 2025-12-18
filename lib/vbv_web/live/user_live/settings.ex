d4378eae3cb739e1e8271504bcd736c8630ed41c
Soren Staun Biangslev <soren.staun@staunstender.com>
2025-12-18 23:59:23 +0100

    Same bug in settings

--- lib/vbv_web/live/user_live/settings.ex
+++ lib/vbv_web/live/user_live/settings.ex
@@ -8,7 +8,7 @@ defmodule VbvWeb.UserLive.Settings do
   @impl true
   def render(assigns) do
     ~H"""
-    <Layouts.app flash={@flash} current_scope={@current_scope} conn={@conn}>
+    <Layouts.app flash={@flash} current_scope={@current_scope}>
       <div class="text-center">
         <.header>
           User Account Settings
