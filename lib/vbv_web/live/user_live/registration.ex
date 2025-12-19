18a7127115241b980215dce596d27c1a338c7865
Soren Staun Biangslev <soren.staun@staunstender.com>
2025-12-19 16:27:13 +0100

    @conn not needed for these views

--- lib/vbv_web/live/user_live/registration.ex
+++ lib/vbv_web/live/user_live/registration.ex
@@ -7,7 +7,7 @@ defmodule VbvWeb.UserLive.Registration do
   @impl true
   def render(assigns) do
     ~H"""
-    <Layouts.app flash={@flash} current_scope={@current_scope} conn={@conn}>
+    <Layouts.app flash={@flash} current_scope={@current_scope}>
       <div class="mx-auto max-w-sm">
         <div class="text-center">
           <.header>
